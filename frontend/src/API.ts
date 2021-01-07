4dece399c59ed4054d544a78ad9eaa91bed02644
Muhibullah Khan Kamali <57056374+Muhibullah-09@users.noreply.github.com>
2021-01-07 23:29:47 +0500

    backend done

---
+++ frontend/src/API.ts
@@ -0,0 +1,46 @@
+/* tslint:disable */
+/* eslint-disable */
+//  This file was automatically generated and should not be edited.
+
+export type LollyInput = {
+  id: string,
+  colorTop: string,
+  colorMiddle: string,
+  colorBottom: string,
+  recipient: string,
+  message: string,
+  sender: string,
+  lollyPath: string,
+};
+
+export type CreateLollyMutationVariables = {
+  lolly?: LollyInput | null,
+};
+
+export type CreateLollyMutation = {
+  createLolly:  {
+    __typename: "Lolly",
+    id: string,
+    colorTop: string,
+    colorMiddle: string,
+    colorBottom: string,
+    recipient: string,
+    message: string,
+    sender: string,
+    lollyPath: string,
+  } | null,
+};
+
+export type GetLolliesQuery = {
+  getLollies:  Array< {
+    __typename: "Lolly",
+    id: string,
+    colorTop: string,
+    colorMiddle: string,
+    colorBottom: string,
+    recipient: string,
+    message: string,
+    sender: string,
+    lollyPath: string,
+  } > | null,
+};
---
+++ frontend/src/graphql/mutations.ts
@@ -0,0 +1,18 @@
+/* tslint:disable */
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const createLolly = /* GraphQL */ `
+  mutation CreateLolly($lolly: LollyInput) {
+    createLolly(lolly: $lolly) {
+      id
+      colorTop
+      colorMiddle
+      colorBottom
+      recipient
+      message
+      sender
+      lollyPath
+    }
+  }
+`;
