83afccd0792fd4a0e1f3637f8c0be0e895d55b3d
Muhibullah Khan Kamali <57056374+Muhibullah-09@users.noreply.github.com>
2021-01-09 15:21:58 +0500

    Update gatsby-config.js

--- lolly-frontend/gatsby-config.js
+++ lolly-frontend/gatsby-config.js
@@ -6,6 +6,9 @@ plugins: [
       typeName: `gatsbyappsync`,
       fieldName: `gatsbyappsync`,
       url: `https://jhgnml6n7ndmfkkzaunzzrdob4.appsync-api.us-east-2.amazonaws.com/graphql`
+    },
+    headers: {
+      'x-api-key': 'randomkey'
     }
   }
 ]
